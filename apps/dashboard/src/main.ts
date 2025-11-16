3592ab491170fdb78f4fc15d8e785576dc4af07f
LaneJS <lanejeremys@gmail.com>
2025-11-16 14:22:32 -0600

    Fix Angular bootstrap platforms

--- apps/dashboard/src/main.ts
+++ apps/dashboard/src/main.ts
@@ -1,7 +1,7 @@
-import { platformBrowser } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app-module';
 
-platformBrowser()
+platformBrowserDynamic()
   .bootstrapModule(AppModule, {
     ngZoneEventCoalescing: true,
   })
